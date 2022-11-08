90b59bfa2633a112060c04df38ee2ef2fbddff75
Prasanna Madusanka <2019cs086@stu.ucsc.lk>
2022-11-08 16:51:31 +0530

    this commit for completing the form

---
+++ README.md
@@ -0,0 +1,5 @@
+# AngularReddit
+
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 10.0.0.
+
+This project follows a tutorial from ng-book Angular 7 (r73) on creating a 'Reddit Clone' using Angular.
---
+++ src/app/article/article.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit, HostBinding, Input } from '@angular/core';
+
+import {Article } from './article.model';
+
+@Component({
+  selector: 'app-article',
+  templateUrl: './article.component.html',
+  styleUrls: ['./article.component.css']
+})
+export class ArticleComponent implements OnInit {
+
+  @HostBinding('attr.class') cssClass = 'card container mb-3';
+  @Input() article: Article;
+
+  constructor() {}
+
+  ngOnInit(): void {
+  }
+
+  voteUp(): boolean {
+    this.article.voteUp();
+    return false;
+  }
+
+  voteDown(): boolean {
+    this.article.voteDown();
+    return false;
+  }
+
+}
